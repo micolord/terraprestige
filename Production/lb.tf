fc8308e3ea50f5c45024082325216cfb907d7b2a
mico <michaelangeloviola@gmauil.com>
2023-05-01 22:39:43 +0800

    added lb

--- Production/lb.tf
+++ Production/lb.tf
@@ -16,7 +16,7 @@ resource "aws_lb_target_group" "alb1-tg" {
   name       = "${var.env_name}-${var.project}-GL-FE-TG"
   port       = 80
   protocol   = "HTTP"
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   slow_start = 0
 
   load_balancing_algorithm_type = "round_robin"
@@ -44,6 +44,10 @@ resource "aws_lb_listener" "alb1-listener" {
   protocol          = "HTTP"
   #ssl_policy        = "ELBSecurityPolicy-2016-08"
   #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb1-tg.arn
+  }
 }
 
 resource "aws_lb_listener_rule" "host_based_routing" {
