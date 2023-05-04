fe572ccd8e835bab259a3cd21ef3907e18098a6b
mico <michaelangeloviola@gmauil.com>
2023-05-05 01:01:28 +0800

    added private rt

--- Production/networking.tf
+++ Production/networking.tf
@@ -87,7 +87,7 @@ resource "aws_route_table" "private1"{
 }
 
 resource "aws_route" "private1_nat_gateway" {
-    route_table_id          = aws_route_table.public.id
+    route_table_id          = aws_route_table.private1.id
     destination_cidr_block  =  "0.0.0.0/0"
     nat_gateway_id              = aws_nat_gateway.nat_gw_1.id
 }
