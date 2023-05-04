8338fca469fce05352bb02918eec86f958908d46
mico <michaelangeloviola@gmauil.com>
2023-05-05 00:56:15 +0800

    added private rt

--- Production/networking.tf
+++ Production/networking.tf
@@ -183,31 +183,6 @@ resource "aws_subnet" "private_subnet8" {
 
 }
 
-resource "aws_route_table" "public"{
-    vpc_id = aws_vpc.vpc.id
-    tags = {
-        Name = "${var.env_name}-${var.project}-public-rt"
-    }
-}
-
-resource "aws_route" "public_internet_gateway" {
-    route_table_id          = aws_route_table.public.id
-    destination_cidr_block  =  "0.0.0.0/0"
-    gateway_id              = aws_internet_gateway.ig.id
-}
-
-resource "aws_route_table_association" "public_1" {
-    subnet_id       =  aws_subnet.public_subnet1.id 
-    route_table_id  = aws_route_table.public.id
-
-}
-
-resource "aws_route_table_association" "public_2" {
-    subnet_id       =  aws_subnet.public_subnet2.id 
-    route_table_id  = aws_route_table.public.id
-
-}
-
 resource "aws_eip" "nat_eip_1" { 
   vpc      = true
 }
