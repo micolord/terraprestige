5412d7bd5741aec7b251b6c410c944474bb25691
mico <michaelangeloviola@gmauil.com>
2023-05-04 23:47:14 +0800

    added ec2 instance and attached iam profile

--- Production/backoffice.tf
+++ Production/backoffice.tf
@@ -22,8 +22,49 @@ resource "aws_iam_role" "backoffice-ssm-role" {
     ]
   })
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  
+  inline_policy {
+    name = "${var.env_name}-${var.project}-iam-pass-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+      {
+            "Sid": "IamPassRole",
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:PassedToService": "ec2.amazonaws.com"
+                }
+            }
+        },
+      ]
+    })
+  }
 
   tags = {
     Name = "${var.env_name}-${var.project}-bo-ssm-role"
+  }
+}
+
+
+resource "aws_instance" "node1" {
+  instance_type          = "c5.large"
+  ami                    = "ami-04f6e8d9cd4ba9e8c"
+  vpc_security_group_ids = [aws_security_group.sg5.id]
+  subnet_id              = aws_subnet.private_subnet5
+
+iam_instance_profile = aws_iam_instance_profile.backoffice-ssm-profile.name
+
+  tags = {
+    Name = "test"
+  }
+
+  #user_data = file("${path.root}/ec2/userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
   }
 }
