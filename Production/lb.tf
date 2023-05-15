7ca8c589ad29661c0db3e000573374fb8143ece2
Paolo <paolo@cloudmade.io>
2023-05-15 15:28:56 +0800

    corrected ssl policy and removed rules

--- Production/lb.tf
+++ Production/lb.tf
@@ -14,8 +14,8 @@ resource "aws_lb" "alb1" {
 
 resource "aws_lb_target_group" "alb1-tg" {
   name       = "${var.env_name}-${var.project}-GL-FE-TG"
-  port       = 443
-  protocol   = "HTTPS"
+  port       = 80
+  protocol   = "HTTP"
   vpc_id     = aws_vpc.vpc.id
   slow_start = 0
 
@@ -28,9 +28,9 @@ resource "aws_lb_target_group" "alb1-tg" {
 
   health_check {
     enabled             = true
-    port                = 443
+    port                = 80
     interval            = 30
-    protocol            = "HTTPS"
+    protocol            = "HTTP"
     path                = "/"
     matcher             = "200-302"
     healthy_threshold   = 3
@@ -40,8 +40,8 @@ resource "aws_lb_target_group" "alb1-tg" {
 
 resource "aws_lb_target_group" "alb1-tg2" {
   name       = "${var.env_name}-${var.project}-GL-BE-TG"
-  port       = 443
-  protocol   = "HTTPS"
+  port       = 80
+  protocol   = "HTTP"
   vpc_id     = aws_vpc.vpc.id
   slow_start = 0
 
@@ -54,9 +54,9 @@ resource "aws_lb_target_group" "alb1-tg2" {
 
   health_check {
     enabled             = true
-    port                = 443
+    port                = 80
     interval            = 30
-    protocol            = "HTTPS"
+    protocol            = "HTTP"
     path                = "/"
     matcher             = "200-302"
     healthy_threshold   = 3
@@ -75,7 +75,7 @@ resource "aws_lb_listener" "alb1-listener" {
     target_group_arn = aws_lb_target_group.alb1-tg.arn
   }
 }
-/*
+
 resource "aws_lb_listener_rule" "host_based_routing" {
   listener_arn = aws_lb_listener.alb1-listener.arn
   priority     = 1
@@ -107,7 +107,7 @@ resource "aws_lb_listener_rule" "host_based_routing2" {
     }
   }
 }
-*/
+
 
 resource "aws_lb" "alb2" {
   name = "${var.env_name}-${var.project}-BackOffice-LB"
@@ -125,8 +125,8 @@ resource "aws_lb" "alb2" {
 
 resource "aws_lb_target_group" "alb2-tg" {
   name       = "${var.env_name}-${var.project}-BO-FE-TG"
-  port       = 443
-  protocol   = "HTTPS"
+  port       = 80
+  protocol   = "HTTP"
   vpc_id     = aws_vpc.vpc.id
   slow_start = 0
 
@@ -139,9 +139,9 @@ resource "aws_lb_target_group" "alb2-tg" {
 
   health_check {
     enabled             = true
-    port                = 443
+    port                = 80
     interval            = 30
-    protocol            = "HTTPS"
+    protocol            = "HTTP"
     path                = "/"
     matcher             = "200-302"
     healthy_threshold   = 3
@@ -151,8 +151,8 @@ resource "aws_lb_target_group" "alb2-tg" {
 
 resource "aws_lb_target_group" "alb2-tg2" {
   name       = "${var.env_name}-${var.project}-BO-BE-TG"
-  port       = 443
-  protocol   = "HTTPS"
+  port       = 80
+  protocol   = "HTTP"
   vpc_id     = aws_vpc.vpc.id
   slow_start = 0
 
@@ -165,9 +165,9 @@ resource "aws_lb_target_group" "alb2-tg2" {
 
   health_check {
     enabled             = true
-    port                = 443
+    port                = 80
     interval            = 30
-    protocol            = "HTTPS"
+    protocol            = "HTTP"
     path                = "/"
     matcher             = "200-302"
     healthy_threshold   = 3
@@ -187,7 +187,7 @@ resource "aws_lb_listener" "alb2-listener" {
   }
 }
 
-/*
+
 resource "aws_lb_listener_rule" "host_based_routing3" {
   listener_arn = aws_lb_listener.alb2-listener.arn
   priority     = 3
@@ -219,5 +219,5 @@ resource "aws_lb_listener_rule" "host_based_routing4" {
     }
   }
 }
-*/
+
 
