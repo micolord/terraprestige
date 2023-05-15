119d131334ab577639f49347ce26b6862309962f
Paolo <paolo@cloudmade.io>
2023-05-15 17:01:59 +0800

    added db source

--- Production/database.tf
+++ Production/database.tf
@@ -56,7 +56,7 @@ resource "aws_db_instance" "master" {
   storage_encrypted           = true
   port                        = "1561"
   vpc_security_group_ids      = [aws_security_group.sg7.id]
-  skip_final_snapshot         = true
+  skip_final_snapshot         = false
 
   timeouts {
     create = "3h"
