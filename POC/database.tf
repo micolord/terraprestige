689ae1a3274739e231f0d9688cebca59d76005c4
mico <michaelangeloviola@gmauil.com>
2023-09-24 23:36:07 +0800

    updated db.tf

--- POC/database.tf
+++ POC/database.tf
@@ -28,7 +28,7 @@ resource "random_password" "master"{
 }
 
 resource "aws_secretsmanager_secret" "masterpassword" {
-  name = "${var.env_name}-${var.project}-master-db-password"
+  name = "${var.env_name}-${var.project}-master-db-passwords"
 }
 
 resource "aws_secretsmanager_secret_version" "password" {
@@ -57,7 +57,7 @@ resource "aws_db_instance" "master" {
   port                        = "1561"
   vpc_security_group_ids      = [aws_security_group.sg7.id]
   final_snapshot_identifier   = "${var.env_name}-${var.project}-master-db-final-snapshot"
-  skip_final_snapshot         = false
+  skip_final_snapshot         = true
   apply_immediately           = true
 
   lifecycle {
